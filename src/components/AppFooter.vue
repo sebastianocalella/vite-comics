6676c4820d83228c8dc4cd82fe51b5dbd5343e9f
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-09 15:54:25 +0100

    footer containers

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,9 @@
 <template lang="">
-    <div>
+    <div class="container">
         <h3>sono un footer</h3>
+        <div class="join-us-banner">
+
+        </div>
     </div>
 </template>
 
@@ -8,6 +11,21 @@
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    .container{
+        background-image: url(../assets/images/footer-bg.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 482px;
 
+        .join-us-banner{
+            height: 80px;
+            width: 100%;
+            background-color: #303030;
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            z-index: 1;
+        }
+    }
 </style>
