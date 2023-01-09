146cc2834b3dcb614205743ba1382fffdabb9616
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-09 15:22:13 +0100

    header basic contents

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,74 @@
 <template lang="">
-    <div>
-        <h1>sono un header</h1>
+    <div class="container">
+        <img src="../assets/images/dc-logo.png" alt="ciao">
+        <nav>
+            <ul>
+                <li v-for="navBarItem in navBarItems">{{navBarItem.text}}</li>
+            </ul>
+        </nav>
     </div>
 </template>
 
 <script>
-
+export default{
+    data() {
+        return{
+            navBarItems: [
+                {
+                    text: "characters"
+                },
+                {
+                    text: "comics"
+                },
+                {
+                    text: "movies"
+                },
+                {
+                    text: "tv"
+                },
+                {
+                    text: "games"
+                },
+                {
+                    text: "collectibles"
+                },
+                {
+                    text: "videos"
+                },
+                {
+                    text: "fans"
+                },
+                {
+                    text: "news"
+                },
+                {
+                    text: "shop"
+                }
+            ]
+        }
+    }
+}
 </script>
 
 <style lang="scss">
-    
+    .container{
+        display: flex;
+        width: 70%;
+        margin: 0 auto;
+        padding: 30px 0;
+        justify-content: space-between;
+
+        ul{
+            display: flex;
+            list-style-type: none;
+            margin: 0;
+            height: 100%;
+
+            li{
+                padding: 0 .4rem;
+                height: 100%;
+                line-height: 6rem;
+            }
+        }
+    }
 </style>
