2c8116305fb4b9932b83daf8b10088d9819acc46
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-10 14:39:45 +0100

    upload comics array in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,7 @@
     <div class="blue-banner">
         <ul>
             <li v-for="blueBarElement in blueBarList">
-                <img :src="getImagePath('../assets/images/'+blueBarElement.image)" alt="blallo">
+                <img :src="getImagePath('../assets/images/'+blueBarElement.imagePath)" alt="blallo">
                 <p>{{blueBarElement.text}}</p>
             </li>
         </ul>
@@ -13,35 +13,109 @@
 </template>
 
 <script>
-export default{
+export default {
     data() {
-        return{
+        return {
             blueBarList: [
                 {
-                    image: 'buy-comics-digital-comics.png',
+                    imagePath: 'buy-comics-digital-comics.png',
                     text: 'digital comics'
                 },
                 {
-                    image: 'buy-comics-merchandise.png',
+                    imagePath: 'buy-comics-merchandise.png',
                     text: 'dc merchandise'
                 },
                 {
-                    image: 'buy-comics-subscriptions.png',
+                    imagePath: 'buy-comics-subscriptions.png',
                     text: 'subscription'
                 },
                 {
-                    image: 'buy-comics-shop-locator.png',
+                    imagePath: 'buy-comics-shop-locator.png',
                     text: 'comic shop locator'
                 },
                 {
-                    image: 'buy-dc-power-visa.svg',
+                    imagePath: 'buy-dc-power-visa.svg',
                     text: 'dc power visa'
+                }
+            ],
+            comics: [
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    price: "$19.99",
+                    series: "Action Comics",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    price: "$3.99",
+                    series: "American Vampire 1976",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    price: "$16.99",
+                    series: "Aquaman",
+                    type: "graphic novel"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                    price: "$2.99",
+                    series: "Batgirl",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    price: "$3.99",
+                    series: "Batman",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    price: "$2.99",
+                    series: "Batman Beyond",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                    price: "$3.99",
+                    series: "Batman/Superman",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                    price: "$4.99",
+                    series: "Batman/Superman Annual",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                    price: "$5.99",
+                    series: "Batman: The Joker War Zone",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                    price: "$6.99",
+                    series: "Batman: Three Jokers",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                    price: "$4.99",
+                    series: "Batman: White Knight Presents: Harley Quinn",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                    price: "$16.99",
+                    series: "Catwoman",
+                    type: "graphic novel"
                 }
             ]
         }
     },
     methods: {
-        getImagePath: function(imgPath){
+        getImagePath: function (imgPath) {
             return new URL(imgPath, import.meta.url).href;
         }
     }
@@ -49,43 +123,42 @@ export default{
 </script>
 
 <style lang="scss" scoped>
-    .black-banner{
+.black-banner {
+    display: flex;
+    align-items: center;
+    background-color: black;
+    padding: 4rem 0;
+    margin: 0 auto;
+
+    h2 {
+        color: white;
+        margin-left: 15%;
+    }
+}
+
+.blue-banner {
+    display: flex;
+    align-items: center;
+    height: 180px;
+    background-color: #0282f9;
+    padding: 0 15%;
+
+    ul {
+        width: 100%;
         display: flex;
-        align-items: center;
-        background-color: black;
-        height: 160px;
-
-        margin: 0 auto;
-
-        h2{
-            color: white;
-            margin-left: 15%;
-        }
+        justify-content: space-around;
     }
 
-    .blue-banner{
+    li {
+        color: white;
         display: flex;
         align-items: center;
-        height: 180px;
-        background-color: #0282f9;
-        padding: 0 15%;
+        text-transform: uppercase;
 
-        ul{
-            width: 100%;
-            display: flex;
-            justify-content: space-around;
-        }
-
-        li{
-            color: white;
-            display: flex;
-            align-items: center;
-            text-transform: uppercase;
-
-            img{
-                height: 50px;
-                margin-right: 10px;
-            }
+        img {
+            height: 50px;
+            margin-right: 10px;
         }
     }
+}
 </style>
